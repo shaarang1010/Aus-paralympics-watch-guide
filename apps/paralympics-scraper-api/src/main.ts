ec894244357efacc6d5ef86a7dda9f3182c2ea1e
Shaarang T <shaarang.1910@gmail.com>
2024-08-18 21:48:10 +1000

    add chromium as core dep

--- apps/paralympics-scraper-api/src/main.ts
+++ apps/paralympics-scraper-api/src/main.ts
@@ -15,7 +15,7 @@ app.get('/api', (req, res) => {
 });
 
 const port = process.env.PORT || 3333;
-const server = app.listen(port, () => {
+const server = app.listen(port, async () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
