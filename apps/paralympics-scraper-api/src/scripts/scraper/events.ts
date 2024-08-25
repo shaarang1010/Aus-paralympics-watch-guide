872552d86f43dfa47d9e1f66a3104aaed98e830a
Shaarang T <shaarang.1910@gmail.com>
2024-08-25 23:11:04 +1000

    revise script

--- apps/paralympics-scraper-api/src/scripts/scraper/events.ts
+++ apps/paralympics-scraper-api/src/scripts/scraper/events.ts
@@ -4,6 +4,7 @@ import type { EventDetails } from '@paralympics-2024/shared-types';
 const DEFAULT_TIMEOUT = 10000;
 const BASE_PARALYMPICS_URL =
   'https://olympics.com/en/paris-2024/paralympic-games/schedule';
+
 export const scrapeEventsSchedule = async (sportsList: string[]) => {
   const browser = await playwright.chromium.launch({ headless: true });
   const context = await browser.newContext(); // launch clean browser context.
@@ -11,6 +12,8 @@ export const scrapeEventsSchedule = async (sportsList: string[]) => {
   const page = await context.newPage(); // create a new page.
 
   let hasAcceptedCookies = false;
+
+  const allParalympicsGameEvents = [];
 
   for (const sport of sportsList) {
     await page.goto(`${BASE_PARALYMPICS_URL}/${sport}`, {
@@ -25,31 +28,72 @@ export const scrapeEventsSchedule = async (sportsList: string[]) => {
       await page.waitForTimeout(DEFAULT_TIMEOUT); // wait for the page to load.
     }
 
-    const eventList = page.locator('[data-cy="events-list"]'); // gets all the events for that sport.
-    const totalEvents = await eventList.count(); // get the total number of events for that sport.
+    const eventsForSport = page.locator('div.competition-day-divider');
 
-    for (let i = 0; i < totalEvents; i++) {
-      const eventDateDetails = eventList.locator(
-        '[data-cy="sport-schedule-date"]'
-      ); // get date details fragment.
-      let eventDay = await eventDateDetails
+    const allEvents = await eventsForSport.count();
+
+    for (let i = 0; i < allEvents; i++) {
+      const eventDate = await eventsForSport
         .nth(i)
-        .locator('span.date-wrapper')
+        .locator('span > time')
         .first()
         .textContent();
-      let eventDate = await eventDateDetails.nth(i).locator('h2').textContent();
-      let eventMonth = await eventDateDetails
-        .nth(i)
-        .locator('span.date-wrapper')
-        .last()
-        .textContent();
 
-      const eventDetailsList = eventList
+      const eventsOnDay = eventsForSport
         .nth(i)
-        .locator('[data-cy="videos-hero]');
-      const totalEventDetails = await eventDetailsList.count();
-      for (let j = 0; j < totalEventDetails; j++) {}
+        .locator("[data-row-type='day-schedule-unit']");
+
+      const allEventsOnDayCount = await eventsOnDay.count();
+
+      for (let j = 0; i < allEventsOnDayCount; j++) {
+        const eventTime = await eventsOnDay
+          .nth(j)
+          .locator("[data-testid='startTime']")
+          .first()
+          .getAttribute('datetime');
+        const eventName = await eventsOnDay
+          .nth(j)
+          .locator('span.discipline-title')
+          .textContent();
+        const eventDescription = await eventsOnDay
+          .nth(j)
+          .locator('span.discipline-sub-title')
+          .textContent();
+        const competitors = await eventsOnDay
+          .nth(j)
+          .locator('div.h2h-competitor')
+          .allTextContents();
+      }
     }
+
+    // const eventList = page.locator('[data-cy="events-list"]'); // gets all the events for that sport.
+    // const totalEvents = await eventList.count(); // get the total number of events for that sport.
+
+    // for (let i = 0; i < totalEvents; i++) {
+    //   const eventDateDetails = eventList.locator(
+    //     '[data-cy="sport-schedule-date"]'
+    //   ); // get date details fragment.
+    //   let eventDay = await eventDateDetails
+    //     .nth(i)
+    //     .locator('span.date-wrapper')
+    //     .first()
+    //     .textContent();
+    //   let eventDate = await eventDateDetails.nth(i).locator('h2').textContent();
+    //   let eventMonth = await eventDateDetails
+    //     .nth(i)
+    //     .locator('span.date-wrapper')
+    //     .last()
+    //     .textContent();
+
+    //   const eventDetailsList = eventList
+    //     .nth(i)
+    //     .locator('[data-cy="videos-hero]');
+
+    //   const totalEventDetails = await eventDetailsList.count();
+    //   for (let j = 0; j < totalEventDetails; j++) {
+    //     const eventDetails = eventDetailsList.nth(j)
+    //   }
+    // }
   }
 
   await browser.close();
